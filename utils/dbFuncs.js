4623b8d22afe8cf1169373f14c9dbbcf4dfd7b51
yevhen_liakh <jeka.lyah1992@gmail.com>
2025-05-30 15:14:52 +0200

    update funcs for pseudo-DB operations

--- utils/dbFuncs.js
+++ utils/dbFuncs.js
@@ -2,6 +2,20 @@ const fs = require('fs').promises;
 const path = require('path');
 const DATA_FILE = path.join(__dirname, '../user_db.json');
 
+/**
+ * @typedef {Object} UserData
+ * @property {string} [first_name] - User's first name
+ * @property {'en'|'nl'|'ua'} [language_code] - User's preferred language
+ * @property {'zwolle'|'apeldoorn'} [direction] - Selected bus direction
+ * @property {string[]} [favorite_stops] - Array of user's favorite bus stops
+ */
+
+/**
+ * Saves or updates user data in the JSON database
+ * @param {string} userId - Telegram user ID
+ * @param {UserData} userData - User data to save
+ * @returns {Promise<void>}
+ */
 async function saveUserData(userId, userData) {
     let allData = {};
     try {
@@ -18,6 +32,11 @@ async function saveUserData(userId, userData) {
     await fs.writeFile(DATA_FILE, JSON.stringify(allData, null, 2));
 }
 
+/**
+ * Retrieves user data from the JSON database
+ * @param {string} [userId] - Telegram user ID. If omitted, returns data for all users
+ * @returns {Promise<UserData|{[key: string]: UserData}|undefined>} User data object or undefined if not found
+ */
 async function loadUserData(userId) {
     try {
         const file = await fs.readFile(DATA_FILE, 'utf8');
